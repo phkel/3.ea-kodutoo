cde7b0243118baf7f96706f801ab505265031012
Kertu Kipper <phkel@users.noreply.github.com>
2018-04-22 12:21:59 +0300

    Add monochrome CSS

--- popup.js
+++ popup.js
@@ -1,18 +1,40 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+/* globals Hls */
 'use strict'
 
-let changeColor = document.getElementById('changeColor')
+const buttonMonochrome = document.getElementById('monochrome')
+const buttonSepia = document.getElementById('sepia')
+const buttonRainbow = document.getElementById('rainbow')
 
-chrome.storage.sync.get('color', function (data) {
-  changeColor.style.backgroundColor = data.color
-  changeColor.setAttribute('value', data.color)
-})
-
-changeColor.onclick = function (element) {
-  let color = element.target.value
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor = "' + color + '";'})
+buttonMonochrome.onclick = function (element) {
+  chrome.tabs.insertCSS(null, {file: 'monochrome.css'})
 }
+
+buttonSepia.onclick = function (element) {
+
+}
+
+buttonRainbow.onclick = function (element) {
+
+}
+
+var video = document.getElementById('video')
+if (Hls.isSupported()) {
+  var hls = new Hls()
+  hls.loadSource('https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8')
+  hls.attachMedia(video)
+  hls.on(Hls.Events.MANIFEST_PARSED, function () {
+    video.play()
+  })
+} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+  video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8'
+  video.addEventListener('canplay', function () {
+    video.play()
+  })
+}
+
+// hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+// When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+// This is using the built-in support of the plain video element, without using hls.js.
